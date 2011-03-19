68cd2b338157d3441eed5b93d35b66b0c08b724b
Pete Marshall <petemarshall77@gmail.com>
2011-03-19 13:13:49 -0700

    Added statis pages html

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,13 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => 
+                                    "Ruby on Rails Tutorial | Home")
+      end
   end
 
   describe "GET 'contact'" do
@@ -15,6 +22,13 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => 
+                                    "Ruby on Rails Tutorial | Contact")
+      end
   end
 
   describe "GET 'about'" do
@@ -22,5 +36,12 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => 
+                                    "Ruby on Rails Tutorial | About")
+      end
   end
 end
