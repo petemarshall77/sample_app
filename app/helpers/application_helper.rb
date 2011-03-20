1967cb146b88c70cda3a045908b4dada0b3b6f8e
Pete Marshall <petemarshall77@gmail.com>
2011-03-19 21:15:39 -0700

    Added link rspecs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Return the page logo
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial"
