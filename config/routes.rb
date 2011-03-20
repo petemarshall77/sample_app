9d1016a83e87b936c1c90baafd1abe9d31d242ff
Pete Marshall <petemarshall77@gmail.com>
2011-03-19 20:57:31 -0700

    Added users controller and signup link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
   root :to => 'pages#home'
 
